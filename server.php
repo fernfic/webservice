38de6349e1a3f33bf1821c6a65845ba8b02ff445
fernfic <a5820097@gmail.com>
2018-10-25 02:43:45 +0700

    add file

--- server.php
+++ server.php
@@ -61,8 +61,8 @@ function set_data($data) {
 }
 function get_data($room) {
     $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b2efec9f22e714', '2d88bcce', 'heroku_c9738a7c9866d40') or die('not connect database'.mysqli_connect_error());
-	mysqli_set_charset($dbcon, 'utf8');
-// 	$query = "SELECT * FROM data_table WHERE room='$room'";
+    mysqli_set_charset($dbcon, 'utf8');
+	// >= '2010-01-31 12:01:01'
 	$query = "SELECT * FROM data_table";
     $result = mysqli_query($dbcon, $query);
     
