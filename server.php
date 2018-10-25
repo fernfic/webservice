6ece661170b58d3160b300a53b6891374475cd54
fernfic <a5820097@gmail.com>
2018-10-25 15:24:45 +0700

    test2

--- server.php
+++ server.php
@@ -21,6 +21,32 @@ $server->wsdl->addComplexType(
     )
 );
 $server->wsdl->addComplexType(
+    'KerryData',
+    'complexType',
+    'struct',
+    'sequence',
+    '',
+    array(
+        'name' => array('name' => 'name', 'type' => 'xsd:string'),
+        'addr' => array('name' => 'addr', 'type' => 'xsd:string'),
+        'weight' => array('name' => 'weight', 'type' => 'xsd:float'),
+    )
+);
+$server->wsdl->addComplexType(
+    'GetKerryData',
+    'complexType',
+    'struct',
+    'sequence',
+    '',
+    array(
+        'name' => array('name' => 'name', 'type' => 'xsd:string'),
+        'addr' => array('name' => 'addr', 'type' => 'xsd:string'),
+        'weight' => array('name' => 'weight', 'type' => 'xsd:float'),
+        'status' => array('name' => 'status', 'type' => 'xsd:string'),
+    )
+);
+
+$server->wsdl->addComplexType(
     'UserData',
     'complexType',
     'struct',
@@ -54,17 +80,26 @@ $server->wsdl->addComplexType(
         'GetUser' => array('name' => 'GetUser','minOccurs'=> '0', 'maxOccurs' =>'unbounded','nillable' => 'true', type=>'tns:UserData')
     )
 );
+$server->wsdl->addComplexType(
+    'GetKerry',
+    'complexType',
+    'struct',
+    'sequence',
+    '',
+    array(
+        'GetKerry' => array('name' => 'GetKerry','minOccurs'=> '0', 'maxOccurs' =>'unbounded','nillable' => 'true', type=>'tns:GetKerryData')
+    )
+);
 
 // Define the method as a PHP function
 function set_data($data) {
-    $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b527b3315d2375', '50a5650c', 'heroku_412cbb6c0f293a3') or die('not connect database'.mysqli_connect_error());
+    $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b2efec9f22e714', '2d88bcce', 'heroku_c9738a7c9866d40') or die('not connect database'.mysqli_connect_error());
     mysqli_set_charset($dbcon, 'utf8');
     $roomid = $data['roomid'];
     $time = $data['time'];
     $temperature = $data['temperature'];
     $humidity = $data['humidity'];
     $query = "INSERT INTO data (roomid, time, temperature, humidity) VALUES('$roomid','$time','$temperature','$humidity')";
-    // $query = "INSERT INTO data_table(room, time, temp, humidity) VALUES('01', '12-09-2016 05:00', '22.5', '10.2')";
     $result = mysqli_query($dbcon, $query);
     mysqli_close($dbcon);
     $send = "add data complete!";
@@ -78,7 +113,7 @@ $server->register('set_data',                    // method name
     'urn:airdata');
 
 function get_data($room) {
-    $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b527b3315d2375', '50a5650c', 'heroku_412cbb6c0f293a3') or die('not connect database'.mysqli_connect_error());
+    $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b2efec9f22e714', '2d88bcce', 'heroku_c9738a7c9866d40') or die('not connect database'.mysqli_connect_error());
     mysqli_set_charset($dbcon, 'utf8');
     $query = "SELECT * FROM data";
     $result = mysqli_query($dbcon, $query);
@@ -98,29 +133,85 @@ $server->register('get_data',                    // method name
     array('room' => 'xsd:string'),
     array('return' => 'tns:GetAir'),    // output parameters
                         'urn:airdata');                  // soapaction
-
+                        
+                        
+//--------------------------test1---------------------------------------//
 function get_user($name) {
-//     $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b2efec9f22e714', '2d88bcce', 'heroku_c9738a7c9866d40') or die('not connect database'.mysqli_connect_error());
-//     mysqli_set_charset($dbcon, 'utf8');
-// 	// >= '2010-01-31 12:01:01'
-// 	$query = "SELECT * FROM data_table";
-//     $result = mysqli_query($dbcon, $query);
-    
-//     if($result){
-//         $data = array();
-// 	    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-// 	        $data[] = array('room'=>$row['room'], 'time'=>$row['time'], 'temp'=>$row['temp'], 'humidity'=>$row['humidity']);
-// 	    }
-//     }
-    // mysqli_close($dbcon);
-    $data = array('name'=> 'Supitcha', 'id'=>'5801012620097', 'hobby'=>array('0'=>'read book', '1'=>'play game'),'sport'=>array('0'=>ball,'1'=>'tennis'));
+    // $hobby = array('0'=>'read book', '1'=>'play game');
+    // $sport = array('0'=>'ball','1'=>'tennis');
+    $hobby = 'read book';
+    $sport = 'ball';
+    $data = array('name'=> 'Supitcha', 'id'=>'5801012620097', 'hobby'=>$hobby,'sport'=>$sport);
     return array('GetUser' => $data);
 }
 
 $server->register('get_user',                    // method name
     array('name' => 'xsd:string'),
     array('return' => 'tns:GetUser'),    // output parameters
-    'urn:airdata');   
+    'urn:airdata'); 
+
+//----------------------------------test2--------------------------------//   
+function send_kerry($data) {
+    $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b2efec9f22e714', '2d88bcce', 'heroku_c9738a7c9866d40') or die('not connect database'.mysqli_connect_error());
+    mysqli_set_charset($dbcon, 'utf8');
+    $name = $data['name'];
+    $addr = $data['addr'];
+    $weight = $data['weight'];
+    $status = "Not Done";
+    $query = "INSERT INTO kerry (name, addr, weight, status) VALUES('$name','$addr','$weight','$status')";
+    $result = mysqli_query($dbcon, $query);
+    mysqli_close($dbcon);
+    $send = "add data complete!";
+    return $send;
+}
+$server->register('send_kerry',                    // method name
+    array('data' => 'tns:KerryData'),
+    array('return' => 'xsd:string'),    // output parameters
+    'urn:airdata'); 
+    
+function update_kerry($data) {
+    $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b2efec9f22e714', '2d88bcce', 'heroku_c9738a7c9866d40') or die('not connect database'.mysqli_connect_error());
+    mysqli_set_charset($dbcon, 'utf8');
+    $name = $data['name'];
+    $addr = $data['addr'];
+    $weight = $data['weight'];
+    $query = "SELECT * FROM kerry WHERE name='$name' and addr = '$addr' and weight = '$weight'";
+    $result = mysqli_query($dbcon, $query);
+    if($result){
+        $query = "UPDATE kerry SET status='Done' WHERE name='$name' and addr = '$addr'";
+        $result = mysqli_query($dbcon, $query);
+        $text = "Update Complete";
+    }else{
+        $text = "No kerry";
+    }
+    
+    mysqli_close($dbcon);
+    return $text;
+}
+$server->register('update_kerry',                    // method name
+    array('data' => 'tns:KerryData'),
+    array('return' => 'xsd:string'),    // output parameters
+    'urn:airdata');
+    
+function get_kerry($name) {
+    $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b2efec9f22e714', '2d88bcce', 'heroku_c9738a7c9866d40') or die('not connect database'.mysqli_connect_error());
+    mysqli_set_charset($dbcon, 'utf8');
+    $query = "SELECT * FROM kerry";
+    $result = mysqli_query($dbcon, $query);
+    if($result){
+        $data = array();
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            $data[] = array('name'=>$row['name'], 'addr'=>$row['addr'], 'weight'=>$row['weight'], 'status'=>$row['status']);
+        }
+    }
+    
+    mysqli_close($dbcon);
+    return array('GetKerry' => $data);
+}
+$server->register('get_kerry',                    // method name
+    array('name' => 'xsd:string'),
+    array('return' => 'tns:GetKerry'),    // output parameters
+    'urn:airdata'); 
 
 @$server->service(file_get_contents("php://input"));
 ?>
