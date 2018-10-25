9b436386fb1c2d251c8ceca4604f88d4d289c49e
fernfic <a5820097@gmail.com>
2018-10-25 13:39:54 +0700

    modified

--- server.php
+++ server.php
@@ -5,6 +5,7 @@ require_once('lib/nusoap.php');
 $server = new soap_server();
 // Initialize WSDL support
 $server->configureWSDL('airdata', 'urn:airdata');
+
 // Register the data structures used by the service
 $server->wsdl->addComplexType(
     'AirData',
@@ -13,12 +14,26 @@ $server->wsdl->addComplexType(
     'sequence',
     '',
     array(
-        'room' => array('name' => 'room', 'type' => 'xsd:string'),
+        'roomid' => array('name' => 'roomid', 'type' => 'xsd:integer'),
         'time' => array('name' => 'time', 'type' => 'xsd:string'),
-        'temp' => array('name' => 'temp', 'type' => 'xsd:float'),
+        'temperature' => array('name' => 'temperature', 'type' => 'xsd:float'),
         'humidity' => array('name' => 'humidity', 'type' => 'xsd:float'),
     )
 );
+$server->wsdl->addComplexType(
+    'UserData',
+    'complexType',
+    'struct',
+    'sequence',
+    '',
+    array(
+        'name' => array('name' => 'name', 'type' => 'xsd:string'),
+        'id' => array('name' => 'id', 'type' => 'xsd:string'),
+        'hobby' => array('name' => 'hobby', 'minOccurs'=> '0', 'maxOccurs' =>'unbounded','nillable' => 'true','type' => 'xsd:string'),
+        'sport' => array('name' => 'sport', 'minOccurs'=> '0', 'maxOccurs' =>'unbounded','nillable' => 'true','type' => 'xsd:string'),
+    )
+);
+
 $server->wsdl->addComplexType(
     'GetAir',
     'complexType',
@@ -29,15 +44,26 @@ $server->wsdl->addComplexType(
         'GetAir' => array('name' => 'GetAir','minOccurs'=> '0', 'maxOccurs' =>'unbounded','nillable' => 'true', type=>'tns:AirData')
     )
 );
+$server->wsdl->addComplexType(
+    'GetUser',
+    'complexType',
+    'struct',
+    'sequence',
+    '',
+    array(
+        'GetUser' => array('name' => 'GetUser','minOccurs'=> '0', 'maxOccurs' =>'unbounded','nillable' => 'true', type=>'tns:UserData')
+    )
+);
 
+// Define the method as a PHP function
 function set_data($data) {
-    $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b2efec9f22e714', '2d88bcce', 'heroku_c9738a7c9866d40') or die('not connect database'.mysqli_connect_error());
-	mysqli_set_charset($dbcon, 'utf8');
-	$room = $data['room'];
-	$time = $data['time'];
-	$temp = $data['temp'];
-	$humidity = $data['humidity'];
-    $query = "INSERT INTO data_table (room, time, temp, humidity) VALUES('$room','$time','$temp','$humidity')";
+    $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b527b3315d2375', '50a5650c', 'heroku_412cbb6c0f293a3') or die('not connect database'.mysqli_connect_error());
+    mysqli_set_charset($dbcon, 'utf8');
+    $roomid = $data['roomid'];
+    $time = $data['time'];
+    $temperature = $data['temperature'];
+    $humidity = $data['humidity'];
+    $query = "INSERT INTO data (roomid, time, temperature, humidity) VALUES('$roomid','$time','$temperature','$humidity')";
     // $query = "INSERT INTO data_table(room, time, temp, humidity) VALUES('01', '12-09-2016 05:00', '22.5', '10.2')";
     $result = mysqli_query($dbcon, $query);
     mysqli_close($dbcon);
@@ -49,32 +75,52 @@ function set_data($data) {
 $server->register('set_data',                    // method name
     array('data' => 'tns:AirData'),          // input parameters
     array('return' => 'xsd:string'),    // output parameters
-    'urn:airdata'                  // soapaction
-);
-
+    'urn:airdata');
 
 function get_data($room) {
-    $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b2efec9f22e714', '2d88bcce', 'heroku_c9738a7c9866d40') or die('not connect database'.mysqli_connect_error());
+    $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b527b3315d2375', '50a5650c', 'heroku_412cbb6c0f293a3') or die('not connect database'.mysqli_connect_error());
     mysqli_set_charset($dbcon, 'utf8');
-	// >= '2010-01-31 12:01:01'
-	$query = "SELECT * FROM data_table";
+    $query = "SELECT * FROM data";
     $result = mysqli_query($dbcon, $query);
-    
     if($result){
         $data = array();
-	    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-	        $data[] = array('room'=>$row['room'], 'time'=>$row['time'], 'temp'=>$row['temp'], 'humidity'=>$row['humidity']);
-	    }
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            $data[] = array('roomid'=>$row['roomid'], 'time'=>$row['time'], 'temperature'=>$row['temperature'], 'humidity'=>$row['humidity']);
+        }
     }
     
     mysqli_close($dbcon);
     return array('GetAir' => $data);
 }
 
+// Register the method to expose
 $server->register('get_data',                    // method name
     array('room' => 'xsd:string'),
     array('return' => 'tns:GetAir'),    // output parameters
-    'urn:airdata');                // soapaction
+                        'urn:airdata');                  // soapaction
+
+function get_user($name) {
+//     $dbcon =  mysqli_connect('us-cdbr-iron-east-01.cleardb.net', 'b2efec9f22e714', '2d88bcce', 'heroku_c9738a7c9866d40') or die('not connect database'.mysqli_connect_error());
+//     mysqli_set_charset($dbcon, 'utf8');
+// 	// >= '2010-01-31 12:01:01'
+// 	$query = "SELECT * FROM data_table";
+//     $result = mysqli_query($dbcon, $query);
+    
+//     if($result){
+//         $data = array();
+// 	    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+// 	        $data[] = array('room'=>$row['room'], 'time'=>$row['time'], 'temp'=>$row['temp'], 'humidity'=>$row['humidity']);
+// 	    }
+//     }
+    // mysqli_close($dbcon);
+    $data = array('name'=> 'Supitcha', 'id'=>'5801012620097', 'hobby'=>array('0'=>'read book', '1'=>'play game'),'sport'=>array('0'=>ball,'1'=>'tennis'));
+    return array('GetUser' => $data);
+}
+
+$server->register('get_user',                    // method name
+    array('name' => 'xsd:string'),
+    array('return' => 'tns:GetUser'),    // output parameters
+    'urn:airdata');   
 
 @$server->service(file_get_contents("php://input"));
 ?>
